a8a632a713d5b728bf40a0d728f72d5a857cb828
Sidhanath Pakhare <sidhpakhare96@gamil.com>
2026-06-10 12:53:05 +0530

    imported modified fixure to block google adds

--- tests/TestCaseTest.spec.ts
+++ tests/TestCaseTest.spec.ts
@@ -1,4 +1,4 @@
-import {expect, test} from '@playwright/test';
+import{test, expect} from '../tests/fixtures';
 import TestCasePage from '../POMClasses/TestCasePage';
 
 test('Verify test case page visibility and test cases count', async ({page}) => {
